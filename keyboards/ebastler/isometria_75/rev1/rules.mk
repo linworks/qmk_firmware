d9dbf9011b529b07e4cfae7d6bfabc00fb65ce1f
Moritz Plattner <moritz.plattner@gmx.net>
2020-08-04 00:59:55 +0200

    add bootmagic lite

--- keyboards/ebastler/isometria_75/rev1/rules.mk
+++ keyboards/ebastler/isometria_75/rev1/rules.mk
@@ -9,6 +9,8 @@ RGBLIGHT_ENABLE = yes
 BACKLIGHT_ENABLE = yes
 ENCODER_ENABLE = yes
 WS2812_DRIVER = pwm
+BOOTMAGIC_ENABLE = lite # Enable lite mode of bootmagic (boot into BL)
+
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
