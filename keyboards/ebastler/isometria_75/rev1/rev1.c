e3aff0c628f33f3d9b425be474a58cc52bcb5b49
Moritz Plattner <moritz.plattner@gmx.net>
2020-09-03 22:26:34 +0200

    fixed suspend

--- keyboards/ebastler/isometria_75/rev1/rev1.c
+++ keyboards/ebastler/isometria_75/rev1/rev1.c
@@ -1,30 +1,5 @@
 #include "rev1.h"
 
-static bool is_suspended, backlight_enabled;
-extern backlight_config_t backlight_config;
-
-
 void matrix_init_kb(void) {
   matrix_init_user();
-}
-
-/* turn backlight (if enabled) off on USB host suspend */
-void suspend_power_down_user(void) {
-#ifdef BACKLIGHT_ENABLE
-    if (!is_suspended) {
-        is_suspended = true;
-        backlight_enabled = backlight_config.enable;
-        backlight_disable();
-    }
-#endif
-}
-
-/* turn backlight (if enabled) on on USB host wakeup */
-void suspend_wakeup_init_user(void) {
-#ifdef BACKLIGHT_ENABLE
-    is_suspended = false;
-    if (backlight_enabled) {
-        backlight_enable();
-    }
-#endif
 }
