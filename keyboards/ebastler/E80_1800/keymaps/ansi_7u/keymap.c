8209175468f1f502f083c44fef8426005e519b73
Moritz Plattner <moritz.plattner@gmx.net>
2020-10-15 16:44:55 +0200

    improved keymap naming

--- keyboards/ebastler/E80_1800/keymaps/ansi_7u/keymap.c
+++ keyboards/ebastler/E80_1800/keymaps/ansi_7u/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,                          KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS,
         KC_CLCK, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                           KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,          KC_KP_1, KC_KP_2, KC_KP_3, KC_PENT,
-        KC_LCTL,          KC_LALT,                            KC_SPC,                    KC_RALT,          LT(1, KC_RCTL),            KC_LEFT, KC_DOWN, KC_RGHT,        KC_KP_0, KC_PCMM),
+        KC_LCTL,          KC_LALT,                            KC_SPC,                    KC_RALT,          LT(1, KC_RCTL),            KC_LEFT, KC_DOWN, KC_RGHT,        KC_KP_0, KC_PDOT),
 
     [1] = LAYOUT_1800_ansi_7u(    /* keymap for layer 1 */
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                   KC_TRNS, KC_TRNS, BL_INC,  KC_TRNS,
